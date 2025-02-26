ab77990317b47a4af52a599b8680e9029323a6c0
Ethan-C11 <ethan.collignon.1102@gmail.com>
2025-02-26 16:35:57 +0100

    fix logs

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,15 @@
 FROM node:22
 
+WORKDIR /app
+
 COPY package.json .
 
 RUN npm install -y
 
 COPY server.js .
+
+RUN mkdir -p /app/logs && chown -R node:node /app/logs
+USER node
 
 EXPOSE 3000
 
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const { createClient } = require('redis');
 const os = require('os');
+const winston = require('winston'), //logger
+    expressWinston = require('express-winston')
 
 const app = express();
 const port = 3000;
+
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.File({ filename: '/app/logs/app.log' }),
+        new winston.transports.Console()
+    ]
+});
+
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: '/app/logs/requests.log' })
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    )
+}));
 
 // Remplacez 'your_redis_ip' et 'your_redis_port' par l'adresse IP et le port de votre serveur Redis
 const redisHost = 'db';
@@ -16,10 +37,12 @@ const redisClient = createClient({
 
 redisClient.on('error', (err) => {
     console.error('Erreur Redis :', err);
+    logger.info('Erreur Redis :', err);
 });
 
 redisClient.on('connect', () => {
     console.log('Connecté à Redis');
+    logger.info('Connecté à Redis');
 });
 
 // Connexion au client Redis
@@ -32,13 +55,14 @@ app.get('/', async (req, res) => {
 app.get('/time', async (req, res) => {
     const currentTime = new Date().toISOString();
     const osType = os.hostname();
-
     try {
         // Enregistrer la date dans Redis
         await redisClient.lPush('times', `${currentTime} - ${osType}`);
         res.send(`The current time is ${currentTime} and the OS is ${osType}`);
+        logger.info(`The current time is ${currentTime} and the OS is ${osType}`);
     } catch (error) {
         res.status(500).send('Erreur lors de l\'enregistrement de la date');
+        logger.info(`Erreur /time`);
     }
 });
 
@@ -47,8 +71,10 @@ app.get('/times', async (req, res) => {
         // Récupérer toutes les dates de Redis
         const times = await redisClient.lRange('times', 0, -1);
         res.send(times);
+        logger.info(times);
     } catch (error) {
         res.status(500).send('Erreur lors de la récupération des dates');
+        logger.info(`Erreur /times`);
     }
 });
 
